6ace026ac21883c13f6607b7ba77779976ed62cc
Manjeet0042 <142335640+Manjeet0042@users.noreply.github.com>
2025-12-28 15:29:56 +0000

    conversion of datatypes

---
+++ 01_basics/03_dataconversion.js
@@ -0,0 +1,36 @@
+let score = "3abc"
+
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// ture => 1; false => 0
+
+// let isLoggedIn = 1
+// let isLoggedIn = ""
+let isLoggedIn = "Manjeet"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn);
+
+// 1 => is converted in true ; 0 => false
+// "" => false
+// "Manjeet" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+

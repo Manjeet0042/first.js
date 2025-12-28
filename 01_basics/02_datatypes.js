9d5e3553878f1286eefd6330f841bc66bfa9b56c
Manjeet0042 <142335640+Manjeet0042@users.noreply.github.com>
2025-12-28 15:10:14 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict";// trat all js code as newer version
+
+//alert ( 3+ 3)// we are using node js, not browser
+
+console.log( 3 +
+     3); // code readability should be high
+
+
+console.log("Manjeet");
+
+
+let name = "Manjeet"
+let age = 18
+let isLoggedIn = false
+
+
+// number => 2 to power 53
+// bigint is used for large number
+// string => ""
+// boolean => true/false
+// null => standloane value / means an empty value
+// undefined => means the value is not defined
+// symbol is used for unique value 
+
+
+// object 
+
+console.log(typeof "Manjeet");

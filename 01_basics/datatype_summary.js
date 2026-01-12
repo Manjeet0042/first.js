1bf04d67abbd81ae4206322598a99274d5cea4f5
Manjeet0042 <142335640+Manjeet0042@users.noreply.github.com>
2026-01-12 15:59:39 +0000

    datatypes_summary

---
+++ 01_basics/datatype_summary.js
@@ -0,0 +1,34 @@
+// primitive datatypes
+// 1 String,2 Number,3 Boolean, 4 null ,5 Symbol ,6 undefined,7 BigInt
+
+// Reference (non primitive datatypes)
+// Array,object,function 
+// the value under carrybracket is known as object 
+// const score = 100 this is number datatypes
+// const scoreValue = 100.3 its also show number datatypes 
+// const isLoggedIn = false its show boolean datatypes 
+// const outsideTemp = null its show object 
+// let userEmail; its show undefined
+// console.log(typeof userEmail);
+
+{
+    // name = "Manjeet Singh" its show String
+    age = 22
+}
+// console.log(typeof name);
+// const heros = ["shaktiman" , "daga"]; its show object 
+
+// all non premitive datatypes are always show objects in its datatype 
+
+
+// console.log(typeof heros);
+
+const myFunction = function(){
+    // console.log("hello world");   its show function 
+
+    
+}
+
+// console.log(typeof myFunction);
+
+
